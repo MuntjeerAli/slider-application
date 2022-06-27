3c3097e7eb200e0b014b484213e2936fe78e60de
MuntjeerAli <muntjeersayyad@gmail.com>
2022-06-27 10:36:01 +0530

    Boiler plate added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className='container'>
+      <div>
+        <h1>My Dream</h1>
+        <div className='underine'></div>
+      </div>
+    </main>
+  )
+}
+
+export default App
