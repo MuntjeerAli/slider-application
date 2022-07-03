ea035bf0b8ac6062d345364f12cd481e622f2125
MuntjeerAli <muntjeersayyad@gmail.com>
2022-07-03 20:20:28 +0530

    Not completed yet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import {FiChevronRight, FiChevronLeft} from 'react-icons/fi';
+import {FaQuoteRight} from 'react-icons/fa';
+
 
 const App = () => {
+  const [dreams, setDreams] = useState(dreams);
+  const [id, setId] = useState(0);
+
   return (
-    <main className='container'>
-      <div>
-        <h1>My Dream</h1>
-        <div className='underine'></div>
+    <section className='section'>
+      <div className='heading'>
+        <h2>
+          My Dreams
+        </h2>
       </div>
-    </main>
+      <div className='center'>
+        {dreams.map((dream) => {
+          const {id, image, name, info} = dream;
+
+          return(
+            <article key={id}>
+              <img src='{image}' alt='{name}' className='dream-img' />
+              <h4>{name}</h4>
+              <p className='text'>{info}</p>
+              <FaQuoteRight className='icon'/>
+            </article>
+          );
+        })}
+
+        <button className='prev'>
+          <FiChevronRight/>
+        </button>
+        <button className='next'>
+          <FiChevronLeft/>
+        </button>
+      </div>
+    </section>
   )
 }
 
